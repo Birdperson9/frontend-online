80cfb7c7ab66c25832505990742d6d329dd42fb2
Birdperson9 <yorieshkin@gmail.com>
2025-01-25 23:34:22 +0100

    add hw1, hw2

---
+++ JS/01/main.js
@@ -0,0 +1,5 @@
+let userAgeValue = 20;
+let userNameNickname = 'John';
+
+userAgeValue = 21;
+userNameNickname = 'Doe';
---
+++ JS/02/main.js
@@ -0,0 +1,32 @@
+let result1 = (5 + 3) * 2 - 7;
+
+let result2 = (10 / 2) ** 2;
+
+let result3 = 2 * (4 + 3) - 8 / 2;
+
+let result4 = (15 % 4) + 5;
+
+let result5 = 3 - 2 ** 4;
+
+console.log(`(5 + 3) * 2 - 7 = ${result1}`);
+console.log(`(10 / 2) ** 2 = ${result2}`);
+console.log(`2 * (4 + 3) - 8 / 2 = ${result3}`);
+console.log(`(15 % 4) + 5 = ${result4}`);
+console.log(`3 - 2 ** 4 = ${result5}`);
+
+let firstVariable = 25;
+let secondVariable = 13;
+
+let result6 = firstVariable + secondVariable;
+let result7 = firstVariable - secondVariable;
+let result8 = firstVariable * secondVariable;
+let result9 = firstVariable / secondVariable;
+let result10 = firstVariable % secondVariable;
+let result11 = firstVariable ** secondVariable;
+
+console.log(`25 + 13 = ${result6}`);
+console.log(`25 - 13 = ${result7}`);
+console.log(`25 * 13 = ${result8}`);
+console.log(`25 / 13 = ${result9}`);
+console.log(`25 % 13 = ${result10}`);
+console.log(`25 ** 13 = ${result11}`);
